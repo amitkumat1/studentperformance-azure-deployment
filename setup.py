b5c3a1a7f08a7a39087db8d4b29923c8185a6399
amitkumat1 <96861691+amitkumat1@users.noreply.github.com>
2026-06-22 14:32:35 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace('\n','') for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+
+setup(
+    name='mlproject26',
+    version='0.0.1',
+    author='Amit',
+    author_email='amitkumars342@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
